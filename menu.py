8f681de7a810bf888b8a067cb9985758b8768375
Macbast <danielbastidas459@gmail.com>
2022-09-17 12:58:26 -0500

    se creo el menu

---
+++ menu.py
@@ -0,0 +1,11 @@
+opcion = 99
+
+while opcion != 3:
+    print("***MENU***")
+    print("1. saludar")
+    print("2. despedir")
+    print("3. salir")
+
+    opcion=int(input("escriba la opcion"))
+else:
+    print("termino")
